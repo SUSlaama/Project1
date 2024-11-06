c4591c6988b1c5417e9d1d5f7fae97b0f82e5c0b
SUSlaama <jakeelperro2003@gmail.com>
2024-11-06 08:25:43 -0600

    First commit

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.project1
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Picture
+import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -24,6 +30,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -46,6 +53,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
@@ -53,35 +61,68 @@ import com.example.project1.ui.theme.Project1Theme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.project1.ui.network.NetworkMonitor
 import com.example.project1.ui.sreens.Components
 import com.example.project1.ui.sreens.HomeScreen
 import com.example.project1.ui.sreens.LoginScreen
 import com.example.project1.ui.sreens.MenuScreen
 
 class MainActivity : ComponentActivity() {
+    private lateinit var wifiManager: WifiManager
+    private lateinit var connectivityManager: ConnectivityManager
+    private lateinit var networkMonitor: NetworkMonitor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        wifiManager = getSystemService(WIFI_SERVICE) as WifiManager
+        connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        networkMonitor = NetworkMonitor(wifiManager, connectivityManager, this)
+
         setContent {
-            ComposeMultiScreenApp()
+            Project1Theme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    networkMonitor.NetworkMonitorScreen()
+                }
+            }
+        }
+
+    }
+
+    fun requestPermissionsIfNeeded() {
+        val permissions = listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ).filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (permissions.isNotEmpty()) {
+            requestPermissionsLauncher.launch(permissions.toTypedArray())
         }
     }
+
+    private val requestPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+            ) {
+                Toast.makeText(this, "Permisos necesarios concedidos", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permisos necesarios no concedidos", Toast.LENGTH_SHORT).show()
+            }
+        }
 }
 
 @Composable
-fun ComposeMultiScreenApp(){
-    val navController = rememberNavController()
-    Surface(color = Color.White) {
-        SetupNavGraph(navController = navController)
-    }
-}
-
-@Composable
-fun SetupNavGraph(navController: NavHostController){
-    NavHost(navController = navController, startDestination = "login"){
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "login") {
         composable("menu") { MenuScreen(navController) }
         composable("home") { HomeScreen(navController) }
-        composable("componentes") { Components(navController)}
-        composable("login") { LoginScreen(navController)}
+        composable("componentes") { Components(navController) }
+        composable("login") { LoginScreen(navController) }
+        //composable("network") { NetworkMonitor(navController)}
     }
 }
